87ef555b3442e49abb74c2d84b73a594d853e827
6fernando9 <fernandopadilla170@gmail.com>
2025-03-02 10:49:05 -0400

    agregando el guard

--- src/app/service/guard.service.ts
+++ src/app/service/guard.service.ts
@@ -1,11 +1,56 @@
+/*
+  CanActivate es una guardia de rutas para restringit el acceso a ciertas rutas de la aplicacion
+  como paginas que requieren autenticacion
+
+  Evalua si el usuario esta autenticado
+  si la condicion es true, permite el acceso a la ruta
+  si es false, redirige a otra pagina
+
+  Similar al Auth de laravel con el guard
+
+
+ */
+
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
-import { Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class GuardService {
+export class GuardService implements CanActivate {
+  //inyeccion de dependencias
+  constructor(private apiService: ApiService, private router: Router) {}
 
-  constructor() { }
+  // tiene 2 parametros
+  //ActivatedRouteSnapshot = contiene informacion de la ruta actual, incluye parametros y datos
+  //RouterStateSnapshot = contiene informacion del estado de la aplicacion y la URL de la ruta solicitada
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    const requiresAdmin = route.data['requiresAdmin'] || false; // en la ruta debe estar definido este parametro data: {requiresAdmin: true}
+
+    if (requiresAdmin) {
+      if (this.apiService.isAdmin()) {
+        return true;
+      } else {
+        //redirigmos a otra ruta
+        this.router.navigate(['/login'], {
+          queryParams: { returnUrl: state.url },
+        });
+        return false;
+      }
+    } else {
+      if (this.apiService.isAuthenticated()) {
+        return true;
+      } else {
+        //redirigmos a otra ruta
+        this.router.navigate(['/login'], {
+          queryParams: { returnUrl: state.url },
+        });
+        return false;
+      }
+    }
+  }
 }
