40edc7864bcd581a4e47cc8675331fab4155fc63
6fernando9 <fernandopadilla170@gmail.com>
2025-03-02 11:15:11 -0400

    creacion del navBar con estilos y agregando metodos en el componente angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+/*
+ChangeDetectorRef
+  es una clase de angular que controla la deteccion de cambios en los componentes
+  se usa para forzar,detener o detectar manualmente cambios en la UI
+
+  casi no es indispensable usar esa clase
+ */
+
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { ApiService } from './service/api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet , RouterLink , CommonModule , ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-front';
+  constructor(private apiService: ApiService,
+     private router: Router,
+    private cdr: ChangeDetectorRef){
+
+  }
+
+  isAuth(){
+    return this.apiService.isAuthenticated();
+  }
+  isAdmin(){
+    return this.apiService.isAdmin();
+  }
+  logout(){
+    this.apiService.logout();
+    this.router.navigate(["/login"]);
+    this.cdr.detectChanges();
+  }
 }
